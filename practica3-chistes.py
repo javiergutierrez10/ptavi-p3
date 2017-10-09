4a5d91ddb953699d5a53985ea66c60705ee9bfa9
Javier  Gutierrez De La Fuente <jguti@gamma26.aulas.gsyc.urjc.es>
2017-10-09 10:56:42 +0200

    Initial commit

--- practica3-chistes.py
+++ practica3-chistes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
+from xml.sax import make_parser #De este sitio importame esta variable
+from xml.sax.handler import ContentHandler #Manejador genérico (Le decimos que hacer cuando se encuentre cosas específicas)
 
 class ChistesHandler(ContentHandler):
     """
@@ -18,6 +18,7 @@ class ChistesHandler(ContentHandler):
         self.inPregunta = False
         self.respuesta = ""
         self.inRespuesta = False
+        self.variable = []
 
     def startElement(self, name, attrs):
         """
@@ -26,8 +27,8 @@ class ChistesHandler(ContentHandler):
         if name == 'chiste':
             # De esta manera tomamos los valores de los atributos
             self.calificacion = attrs.get('calificacion', "")
+            self.variable.append(self.calificacion)
         elif name == 'pregunta':
-            print("hola")
             self.inPregunta = True
         elif name == 'respuesta':
             self.inRespuesta = True
@@ -37,9 +38,11 @@ class ChistesHandler(ContentHandler):
         Método que se llama al cerrar una etiqueta
         """
         if name == 'pregunta':
+            self.variable.append(self.pregunta)
             self.pregunta = ""
             self.inPregunta = False
         if name == 'respuesta':
+            self.variable.append(self.respuesta)
             self.respuesta = ""
             self.inRespuesta = False
 
@@ -48,15 +51,18 @@ class ChistesHandler(ContentHandler):
         Método para tomar contenido de la etiqueta
         """
         if self.inPregunta:
-            self.pregunta = self.pregunta + char
+            self.pregunta = self.pregunta + char #equivalente (concatena)
         if self.inRespuesta:
-            self.respuesta += char
+            self.respuesta += char #equivalente
 
 if __name__ == "__main__":
     """
     Programa principal
     """
-    parser = make_parser()
-    cHandler = ChistesHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open('chistes2.xml'))
+    parser = make_parser() #Salto línea a línea
+    cHandler = ChistesHandler() #Maneja los chistes
+    parser.setContentHandler(cHandler) #ligar el parser con el manejador
+    parser.parse(open('chistes2.xml')) #parseame el archivo
+    print(cHandler.variable)
+
+
--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,82 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler):
+
+    def __init__ (self):
+#   Constructor. Inicializamos las variables
+
+        self.width = ""
+        self.height = ""
+        self.background_color = ""
+        self.id = ""
+        self.top = ""
+        self.bottom = ""
+        self.left = ""
+        self.right = ""
+        self.src = ""
+        self.region = ""
+        self.begin = ""
+        self.dur = ""
+        
+        self.variable = []
+        
+    def startElement(self, name, attrs):
+
+        if name == 'root_layout':
+            self.variable.append('root_layout')
+            
+            self.width = attrs.get('width', "")
+            self.height = attrs.get('heigth', "")
+            self.background_color = attrs.get('background_color', "")
+            self.variable.append(self.width)
+            self.variable.append(self.height)
+            self.variable.append(self.background-color)    
+        elif name == 'region':
+            self.variable.append('region')
+            self.id = attrs.get('id', "")
+            self.top = attrs.get('top', "")
+            self.bottom = attrs.get('bottom', "")
+            self.left = attrs.get('left', "")
+            self.right = attrs.get('right', "")
+            self.variable.append(self.id)
+            self.variable.append(self.top)
+            self.variable.append(self.bottom)
+            self.variable.append(self.letf)
+            self.variable.append(self.right)             
+        elif name == 'img':
+            self.variable.append('img')
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+            self.variable.append(self.src)
+            self.variable.append(self.region)
+            self.variable.append(self.begin)
+            self.variable.append(self.dur)     
+        elif name == 'audio':
+            self.variable.append('audio')
+            self.src = attrs.get('src', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+            self.variable.append(self.src)
+            self.variable.append(self.begin)
+            self.variable.append(self.dur) 
+        elif name == 'textstream':
+            self.variable.append('textstream')
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.variable.append(self.src)
+            self.variable.append(self.region)
+            
+if __name__ == "__main__":
+
+    parser = make_parser() #Salto línea a línea
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler) #ligar el parser con el manejador
+    parser.parse(open('karaoke.smil')) #parseame el archivo
+    print(cHandler.variable)
 
